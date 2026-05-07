3b0bbf80954e57d02ae80fd4a12ef3666b576c35
alpiventra <118706057+satriadaffa@users.noreply.github.com>
2026-05-07 20:12:04 +0700

    Initialize database in main execution context

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ with app.app_context():
     init_db()
 
 if __name__ == '__main__':
+    with app.app_context():
     init_db()
     # Railway menyediakan PORT melalui environment variable
     # Fallback ke 5000 saat dijalankan secara lokal
